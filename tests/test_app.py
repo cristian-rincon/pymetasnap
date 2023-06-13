1739ec1d419177d87ea3a46ad9232e490f5016cc
CRRINCO <epam.rincon@mbusa.com>
2023-06-13 16:58:13 -0500

    test: first test created

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from typer.testing import CliRunner
+
+from extractor.main import app
+
+runner = CliRunner()
+
+def test_app():
+    result = runner.invoke(app, ["version"])
+    assert result.exit_code == 0
+    assert "0.1.0\n" == result.stdout
